709ac5e51e5f9d3950f9790cf6f50a437b74b9d5
BlueHorizon186 <bluehorizon186@gmail.com>
2016-11-05 22:08:04 -0500

    Application now reads the Matches Calendar from the organization's server.

--- app/src/main/java/mx/itesm/cem/mmf/protoconadeip/Pant2.java
+++ app/src/main/java/mx/itesm/cem/mmf/protoconadeip/Pant2.java
@@ -6,16 +6,56 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+
+import mx.itesm.cem.mmf.protoconadeip.corelogic.CalendarFetcher;
+import mx.itesm.cem.mmf.protoconadeip.corelogic.coreconstants.CalendarData;
 
 /**
- * Created by sandWolf on 06/10/2016.
+ * Created on 06/10/2016.
  */
 public class Pant2 extends Fragment {
-    String[] opciones = {"X1","X2"};
+
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.pag2,container,false);
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
 
+        View screenRootView = inflater.inflate(R.layout.pag2, container, false);
+        CalendarFetcher fetcher = new CalendarFetcher();
+        String[] values = {"Error al cargar el calendario actual."};
+
+        try {
+            JSONArray calendarArr = fetcher.execute(15).get();
+            ArrayList<String> calendarAsLst = new ArrayList<>();
+
+            for (int i = 0; i < calendarArr.length(); i++) {
+                JSONArray next = calendarArr.getJSONArray(i);
+                String game = next.getString(CalendarData.HOME_TEAM.num()) + " "
+                        + next.getString(CalendarData.VISIT_TEAM.num()) + " "
+                        + next.getString(CalendarData.DATE.num()) + " "
+                        + next.getInt(CalendarData.HOME_SCORE.num()) + " "
+                        + next.getInt(CalendarData.VISIT_SCORE.num());
+                calendarAsLst.add(game);
+            }
+
+            values = calendarAsLst.toArray(new String[calendarAsLst.size()]);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
+                android.R.layout.simple_list_item_1, values);
+
+        ListView lv = (ListView) screenRootView.findViewById(R.id.listView2);
+        lv.setAdapter(adapter);
+        return screenRootView;
     }
 }
---
+++ app/src/main/java/mx/itesm/cem/mmf/protoconadeip/corelogic/CalendarFetcher.java
@@ -0,0 +1,36 @@
+package mx.itesm.cem.mmf.protoconadeip.corelogic;
+
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ * Created on 05/11/16.
+ */
+
+public class CalendarFetcher extends AsyncTask<Integer, Void, JSONArray> {
+
+    private static JSONReader jsonReader = new JSONReader();
+
+    private JSONArray fetchCalendar(int category) {
+        String calendarUrl = "http://app.conadeipfba.org.mx/"
+                + "php/getMatches.php?categoriaId=" + category;
+
+        try {
+            JSONObject calendarObj = new JSONObject(
+                    jsonReader.readJSONFromServer(calendarUrl));
+            return calendarObj.getJSONArray("matchesInfo");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected JSONArray doInBackground(Integer... params) {
+        int cat = params[0];
+        return fetchCalendar(cat);
+    }
+}
