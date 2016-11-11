07248bceed75d5007edfd92f8c344c7c45c4c991
BlueHorizon186 <bluehorizon186@gmail.com>
2016-11-10 21:56:10 -0600

    Refactored in Main Activity.

--- app/src/main/java/mx/itesm/cem/mmf/protoconadeip/MainActivity.java
+++ app/src/main/java/mx/itesm/cem/mmf/protoconadeip/MainActivity.java
@@ -5,19 +5,15 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     String[] opciones2 = {"X1","X2"};
-    TabLayout tL;
-    ViewPager vP;
+    TabLayout tabLayout;
+    ViewPager viewPager;
     Pant1 p1 = new Pant1();
     Pant2 p2 = new Pant2();
     Pant3 p3 = new Pant3();
@@ -28,47 +24,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        vP = (ViewPager) findViewById(R.id.viewPager);
-        vP.setAdapter(new CustomAdapter(getSupportFragmentManager(),getApplicationContext()));
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPager.setAdapter(new CustomAdapter(getSupportFragmentManager(),getApplicationContext()));
 
-        tL = (TabLayout) findViewById(R.id.tabLayout);
-        tL.setupWithViewPager(vP);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
 
-        tL.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                vP.setCurrentItem(tab.getPosition());
+                viewPager.setCurrentItem(tab.getPosition());
             }
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
-                vP.setCurrentItem(tab.getPosition());
+                viewPager.setCurrentItem(tab.getPosition());
             }
 
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
-                vP.setCurrentItem(tab.getPosition());
+                viewPager.setCurrentItem(tab.getPosition());
             }
         });
     }
 
     private class CustomAdapter extends FragmentPagerAdapter {
-        private String frag []={"Posiciones", "Calendario", "Configuracion"};
+        private String frag [] = {"Posiciones", "Calendario", "Configuracion"};
 
         public CustomAdapter(FragmentManager supportFragmentManager, Context applicationContext) {
             super(supportFragmentManager);
         }
 
         @Override
-        public Fragment getItem(int position) {
-            if(position==0){
-                return p1;
-            }else if(position==1){
-                return p2;
-            }else if(position==2){
-                return p3;
-            }else{
-            return null;}
+        public Fragment getItem(int index) {
+            switch (index) {
+                case 0:
+                    return p1;
+                case 1:
+                    return p2;
+                case 2:
+                    return p3;
+                default:
+                    return null;
+            }
         }
 
         @Override
