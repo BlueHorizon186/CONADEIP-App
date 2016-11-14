899ac226caaca2ed0adf7222ed391686fc3aa1a3
BlueHorizon186 <bluehorizon186@gmail.com>
2016-11-13 20:34:51 -0600

    Calendar is now displayed with Conadeip's information.

--- app/src/main/java/mx/itesm/cem/mmf/protoconadeip/Pant2.java
+++ app/src/main/java/mx/itesm/cem/mmf/protoconadeip/Pant2.java
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import org.json.JSONArray;
@@ -14,7 +13,6 @@ import org.json.JSONArray;
 import java.util.ArrayList;
 
 import mx.itesm.cem.mmf.protoconadeip.corelogic.CalendarFetcher;
-import mx.itesm.cem.mmf.protoconadeip.corelogic.coreconstants.CalendarData;
 import mx.itesm.cem.mmf.protoconadeip.corelogic.coreuiadapters.CalendarListAdapter;
 
 /**
@@ -29,28 +27,23 @@ public class Pant2 extends Fragment {
                              @Nullable Bundle savedInstanceState) {
 
         View screenRootView = inflater.inflate(R.layout.pag2, container, false);
-        //CalendarFetcher fetcher = new CalendarFetcher();
-        String[] values = {"Error al cargar el calendario actual."};
+        CalendarFetcher fetcher = new CalendarFetcher();
+        //String[] values = {"Error al cargar el calendario actual."};
+        JSONArray[] values = null;
 
-        /*try {
+        try {
             JSONArray calendarArr = fetcher.execute(15).get();
-            ArrayList<String> calendarAsLst = new ArrayList<>();
+            ArrayList<JSONArray> calendarAsLst = new ArrayList<>();
 
             for (int i = 0; i < calendarArr.length(); i++) {
                 JSONArray next = calendarArr.getJSONArray(i);
-                String game = next.getString(CalendarData.HOME_TEAM.num()) + " "
-                        + next.getString(CalendarData.VISIT_TEAM.num()) + " "
-                        + next.getString(CalendarData.DATE.num()) + " "
-                        + next.getInt(CalendarData.HOME_SCORE.num()) + " "
-                        + next.getInt(CalendarData.VISIT_SCORE.num());
-                calendarAsLst.add(game);
+                calendarAsLst.add(next);
             }
-
-            values = calendarAsLst.toArray(new String[calendarAsLst.size()]);
+            values = calendarAsLst.toArray(new JSONArray[calendarAsLst.size()]);
 
         } catch (Exception e) {
             e.printStackTrace();
-        }*/
+        }
 
         CalendarListAdapter adapter =
                 new CalendarListAdapter(getActivity(), values);
--- app/src/main/java/mx/itesm/cem/mmf/protoconadeip/corelogic/coreuiadapters/CalendarListAdapter.java
+++ app/src/main/java/mx/itesm/cem/mmf/protoconadeip/corelogic/coreuiadapters/CalendarListAdapter.java
@@ -5,21 +5,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 
 import mx.itesm.cem.mmf.protoconadeip.R;
+import mx.itesm.cem.mmf.protoconadeip.corelogic.coreconstants.CalendarData;
 
 /**
  * Created on 13/11/16.
  */
 
-public class CalendarListAdapter extends ArrayAdapter<String> {
+public class CalendarListAdapter extends ArrayAdapter<JSONArray> {
 
     private final Activity context;
-    private final String[] itemName;
+    private final JSONArray[] itemName;
 
-    public CalendarListAdapter(Activity context, String[] itemName) {
+    public CalendarListAdapter(Activity context, JSONArray[] itemName) {
         super(context, R.layout.calendar_listrow_layout, itemName);
         this.context = context;
         this.itemName = itemName;
@@ -28,6 +30,59 @@ public class CalendarListAdapter extends ArrayAdapter<String> {
     public View getView(final int index, View view, ViewGroup parent) {
         LayoutInflater inflater = context.getLayoutInflater();
         View rowView = inflater.inflate(R.layout.calendar_listrow_layout, null, true);
+        JSONArray next = itemName[index];
+
+        TextView homeTeamView = (TextView) rowView.findViewById(R.id.homeTeamValue);
+        TextView visitTeamView = (TextView) rowView.findViewById(R.id.visitTeamValue);
+        TextView homeScoreView = (TextView) rowView.findViewById(R.id.homeScoreValue);
+        TextView visitScoreView = (TextView) rowView.findViewById(R.id.visitScoreValue);
+
+        TextView gameStatusView = (TextView) rowView.findViewById(R.id.gameStatusValue);
+        TextView gameDateView = (TextView) rowView.findViewById(R.id.gameDateValue);
+        TextView gameTimeView = (TextView) rowView.findViewById(R.id.gameTimeValue);
+
+        try {
+            String gameStatus = "";
+
+            homeTeamView.setText(next.getString(CalendarData.HOME_TEAM.num()));
+            visitTeamView.setText(next.getString(CalendarData.VISIT_TEAM.num()));
+            homeScoreView.setText(next.getString(CalendarData.HOME_SCORE.num()));
+            visitScoreView.setText(next.getString(CalendarData.VISIT_SCORE.num()));
+
+            gameDateView.setText(next.getString(CalendarData.DATE.num()));
+            gameTimeView.setText(next.getString(CalendarData.TIME.num()));
+
+            switch (next.getInt(CalendarData.STATUS.num())) {
+                case 8:
+                    gameStatus = "Programado";
+                    break;
+                case 6:
+                    gameStatus = "Descanso";
+                    break;
+                case 5:
+                    gameStatus = "Terminado";
+                    break;
+                case 4:
+                    gameStatus = "Ultimo Cuarto";
+                    break;
+                case 3:
+                    gameStatus = "Tercer Cuarto";
+                    break;
+                case 2:
+                    gameStatus = "Segundo Cuarto";
+                    break;
+                case 1:
+                    gameStatus = "Primer Cuarto";
+                    break;
+                default:
+                    gameStatus = "Estado Desconocido";
+                    break;
+            }
+            gameStatusView.setText(gameStatus);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return rowView;
     }
 }
